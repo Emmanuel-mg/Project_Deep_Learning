d45d67e099c2f6acf0c81e7952d1e4da9efab69e
Emmanuel-mg <emmanuel.minois-genin@ecl20.ec-lyon.fr>
2023-11-06 16:55:18 +0100

    We can now work on model.py

--- PaiNN/PaiNN/model/model.py
+++ PaiNN/PaiNN/model/model.py
@@ -1,1 +1,12 @@
 import torch
+import torch.nn as nn
+
+
+class PaiNNModel(nn.Module):
+    def __init__(self):
+        embedding_layer = nn.Embedding()
+
+    def forward(self, *input):
+        """ Forward pass logic """
+
+        raise NotImplementedError
