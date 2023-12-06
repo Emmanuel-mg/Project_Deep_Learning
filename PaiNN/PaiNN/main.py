4725d77962a651e1d13a4cb967da6051308179f8
Emmanuel-mg <emmanuel.minois-genin@ecl20.ec-lyon.fr>
2023-12-06 19:23:52 +0100

    chzngr erpoch

--- PaiNN/PaiNN/main.py
+++ PaiNN/PaiNN/main.py
@@ -27,7 +27,7 @@ def training():
             scheduler=scheduler,
             device=device
         )
-        trainer._train(num_epoch = 10, epoch_swa = 1, early_stopping = 30)
+        trainer._train(num_epoch = 1, epoch_swa = 0, early_stopping = 30)
         trainer.plot_data()
 
 if __name__=="__main__":
